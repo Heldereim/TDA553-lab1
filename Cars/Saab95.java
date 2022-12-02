9652cdd4cdbf66c00759604964582eaa7e62ad6c
kalimooo <kallewiklund2002@gmail.com>
2022-12-02 10:05:46 +0100

    Update Saab95.java

--- Cars/Saab95.java
+++ Cars/Saab95.java
@@ -1,6 +1,6 @@
 import java.awt.*;
 
-public class Saab95<TE, B> extends MotorisedVehicle<Engine, Body> {
+public class Saab95 extends MotorisedVehicle<TurboEngine, Body> {
 
     private boolean turboOn;
     
