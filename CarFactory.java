00da83ed48f5d9582736c1b3c4217b254c5a9d8f
kalimooo <kallewiklund2002@gmail.com>
2022-12-08 15:52:47 +0100

    trying to make factory

--- CarFactory.java
+++ CarFactory.java
@@ -1,18 +1,63 @@
-import java.util.ArrayList;
+import java.util.*;
+import java.awt.Color;
 
 public class CarFactory {
 
-    public ArrayList<MotorisedVehicle<?, ?>> allVehicles;
-    public ArrayList<Saab95> saabList;
-    public ArrayList<Scania> scaniaList;
+    private ArrayList<Saab95> saabList;
+    private ArrayList<Scania> scaniaList;
+    private ArrayList<Volvo240> volvoList;
+    private ArrayList<ArrayList<MotorisedVehicle<?, ?>>> carList;
+
+    public CarFactory() {
+        this.saabList = new ArrayList<Saab95>();
+        this.scaniaList = new ArrayList<Scania>();
+        this.volvoList = new ArrayList<Volvo240>();
+        this.carList = new ArrayList<ArrayList<MotorisedVehicle<?, ?>>>();
+
+        this.carList.add(this.saabList); // TODO fucked because of 
+
+    }
 
     public void makeSaab() {
         Saab95 saab = new Saab95();
+        saabList.add(saab);
+    }
+
+    public void makeSaab(TurboEngine engine, Body body) {
+        Saab95 saab = new Saab95(engine, body);
+        allVehicles.add(saab);
+        saabList.add(saab);
+    }
+
+    public void makeSaab(double enginePower, String modelName, Color color, int nrDoors) {
+        Saab95 saab = new Saab95(enginePower, modelName, color, nrDoors);
         allVehicles.add(saab);
         saabList.add(saab);
     }
 
     public void makeVolvo() {
+        Volvo240 volvo = new Volvo240();
+        allVehicles.add(volvo);
+    }
 
+    public void makeVolvo(Engine engine, TrimBody body) {
+        Volvo240 volvo = new Volvo240(engine, body);
+        allVehicles.add(volvo);
+    }
+
+    public void makeVolvo(double enginePower, String modelName, Color color, int nrDoors, double trimFactor) {
+        Volvo240 volvo = new Volvo240(enginePower, modelName, color, nrDoors, trimFactor);
+        allVehicles.add(volvo);
+    }
+
+    public void makeScania() {
+        Scania scania = new Scania();
+        scaniaList.add(scania);
+        allVehicles.add(scania);
+    }
+
+    public void makeCarTransporter(int maxCapacity) {
+        CarTransporter carTransporter = new CarTransporter(maxCapacity);
+        allVehicles.add(carTransporter);
     }
 }
--- Cars/Car.java
+++ Cars/Car.java
@@ -1,5 +1,1 @@
-public abstract class Car<E extends Engine, B extends Body> extends MotorisedVehicle<E, B> {
-    public Car(E engine, B body) {
-        super(engine, body);
-    }
-}
+public abstract class Car<E extends Engine, B extends Body> extends MotorisedVehicle<E, B> {}
